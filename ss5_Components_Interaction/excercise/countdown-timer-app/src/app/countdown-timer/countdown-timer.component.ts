ce633cd3153929373030f59691f4bbc1b9967a46
lexuanvu <vulex289@gmail.com>
2023-03-09 17:18:06 +0700

    bt2

---
+++ ss5_Components_Interaction/excercise/countdown-timer-app/src/app/countdown-timer/countdown-timer.component.ts
@@ -0,0 +1,67 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-timer',
+  templateUrl: './countdown-timer.component.html',
+  styleUrls: ['./countdown-timer.component.css']
+})
+export class CountdownTimerComponent implements OnInit {
+
+  constructor() { }
+
+
+
+  message = '';
+  remainingTime: number;
+  @Input()
+  seconds = 11;
+  @Output()
+  finish = new EventEmitter<boolean>();
+  private intervalId = 0;
+
+
+
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  ngOnInit() {
+    this.reset();
+  }
+
+  ngOnDestroy() {
+    this.clearTimer();
+  }
+
+  start() {
+    this.countDown();
+    if (this.remainingTime <= 0) {
+      this.remainingTime = this.seconds;
+    }
+  }
+
+  stop() {
+    this.clearTimer();
+    this.message = `Holding at T-${this.remainingTime} seconds`;
+  }
+
+  reset() {
+    this.clearTimer();
+    this.remainingTime = this.seconds;
+    this.message = `Click start button to start the Countdown`;
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.remainingTime -= 1;
+      if (this.remainingTime === 0) {
+        this.message = 'Blast off!';
+        this.clearTimer();
+        this.finish.emit(true);
+      } else {
+        this.message = `T-${this.remainingTime} seconds and counting`;
+      }
+    }, 1000);
+  }
+}
